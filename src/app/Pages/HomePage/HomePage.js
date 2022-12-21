a5ee562d42d106845a1403d3b8dd4e483d4b0d26
KarolisPo <karo.pociu@gmail.com>
2022-12-21 18:49:33 +0200

    homepage to functions

--- src/app/Pages/HomePage/HomePage.js
+++ src/app/Pages/HomePage/HomePage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import Header from "../../components/Header"
+import React, { useCallback, useEffect, useState} from 'react';
+import Header from '../../components/Header';
 import Button from '../../components/Button';
 import Hero from '../../components/Hero';
 import Card from '../../components/MovieCard';
@@ -9,84 +9,67 @@ import './HomePage.css';
 
 import Logo from '../../images/F.png';
 
-class HomePage extends React.Component {
-  state = {
-    loading: false,
-    error: false,
-    movies: [],
-    favoriteMovies: [],
-  };
-  
-  async componentDidMount() {
-    this.setState({ loading: true });
+function HomePage({ setFavorite, favoriteMovies }) {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(false);
 
+  const getData = useCallback(async () => {
     try {
-      if (localStorage.getItem('favoriteMovies')) {
-        const favoriteMovies = JSON.parse(
-          localStorage.getItem('favoriteMovies')
-        );
-        this.setState({ favoriteMovies });
-      }
 
       const result = await fetch(
         'https://dummy-video-api.onrender.com/content/free-items'
       );
       if (result.status >= 400 && result.status <= 599) {
-        this.setState({ error: true });
+        setError(true);
       } else {
         const movies = await result.json();
-        this.setState({ movies: movies });
+        setMovies(movies);
       }
     } catch (error) {
-      this.setState({ error: true });
+      setError(true);
     } finally {
-      this.setState({ loading: false });
+      setError(false);
     }
-  }
+  }, []);
 
-  render() {
-    const {setFavorite, favoriteMovies} = this.props;
-    const {movies} = this.state;
+  useEffect(() => {
+    getData();
+  }, [getData]);
 
-    return (
-      <div className="App">
-        <Header logoImg={Logo} imgAlt="logo image">
-          <Button btnStyle="Red-btn" to="/signin">Sign In</Button>
-        </Header>
-        <Hero title="Wanna more content?">
-          <Button btnStyle="Red-btn">Get Access</Button>
-        </Hero>
-        <div className="Gray-line"></div>
-        <main className="Content">
-          <div className="Cards-wraper">
-            {movies.map(({ title, id, description, image }, index) => (
-              <Card
-                title={title}
-                description={description}
-                src={image}
-                alt={title}
-                key={index}
-              >
-                <Button
-                  btnStyle="Card-btn"
-                  onClick={setFavorite}
-                  id={id}
-                >
-                  {favoriteMovies.includes(id)
-                    ? 'Remove'
-                    : 'Favorite'}
-                </Button>
-              </Card>
-            ))}
-          </div>
-          <div className="Btn-wrap">
-            <Button btnStyle="Red-btn">Get More Content</Button>
-          </div>
-        </main>
-        <Footer></Footer>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <Header logoImg={Logo} imgAlt="logo image">
+        <Button btnStyle="Red-btn" to="/signin">
+          Sign In
+        </Button>
+      </Header>
+      <Hero title="Wanna more content?">
+        <Button btnStyle="Red-btn">Get Access</Button>
+      </Hero>
+      <div className="Gray-line"></div>
+      <main className="Content">
+        <div className="Cards-wraper">
+          {movies.map(({ title, id, description, image }, index) => (
+            <Card
+              title={title}
+              description={description}
+              src={image}
+              alt={title}
+              key={index}
+            >
+              <Button btnStyle="Card-btn" onClick={setFavorite} id={id}>
+                {favoriteMovies.includes(id) ? 'Remove' : 'Favorite'}
+              </Button>
+            </Card>
+          ))}
+        </div>
+        <div className="Btn-wrap">
+          <Button btnStyle="Red-btn">Get More Content</Button>
+        </div>
+      </main>
+      <Footer></Footer>
+    </div>
+  );
 }
 
 export default HomePage;
